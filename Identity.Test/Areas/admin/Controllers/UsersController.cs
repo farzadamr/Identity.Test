9bf9687d039a7fb46c357dd9ff64389079253fe5
Farzad <amirifarzad2002@gmail.com>
2024-08-11 19:49:53 +0330

    Complete UserList Section and Admin Area

---
+++ Identity.Test/Areas/admin/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using Identity.Test.Areas.admin.Models;
+using Identity.Test.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Test.Areas.admin.Controllers
+{
+    [Area("admin")]
+    public class UsersController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        public UsersController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            var Users = _userManager.Users.Select(p => new UserListDto()
+            {
+                Id = p.Id,
+                UserName = p.UserName,
+                Email = p.Email,
+                FirstName = p.FirstName,
+                LastName = p.LastName,
+                AccessFailedCount = p.AccessFailedCount
+            }).ToList();
+            
+
+            return View(Users);
+        }
+    }
+}
---
+++ Identity.Test/Areas/admin/Models/UserListDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Identity.Test.Areas.admin.Models
+{
+    public class UserListDto
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int AccessFailedCount { get; set; }
+
+    }
+}
